8997041addee60180798dc6fb01f000ac4f0327f
SGpirr <binhoo4132@gmail.com>
2023-05-31 21:27:20 +0900

    배포 기본 포트 8889 -> 40071 변경

--- deploy/fast.py
+++ deploy/fast.py
@@ -54,4 +54,4 @@ def ping():
 import pathlib
 import uvicorn
 if __name__ == "__main__":
-    uvicorn.run('__main__:app', host="0.0.0.0", port=8889)
+    uvicorn.run('__main__:app', host="0.0.0.0", port=40071)
